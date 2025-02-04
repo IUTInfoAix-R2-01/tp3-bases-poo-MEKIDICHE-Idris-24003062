df5020c4f7a37a98ace4c08a0831d48d2d3326ef
Mekidiche <MEKIDICHE-Idris-24003062>
2025-02-04 17:30:11 +0100

    invoiceitem le 1.5

---
+++ src/invoiceitem/invoiceitem.java
@@ -0,0 +1,14 @@
+package invoiceitem;
+
+public class invoiceitem {
+	private String id;
+	private String desc;
+	private int qty;
+	private double unitPrice;
+	
+	public Employee (String id, String desc, int qty, double unitPrice){
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.salary = salary;
+}
---
+++ src/invoiceitem/invoiceitemtest.java
@@ -0,0 +1,10 @@
+package invoiceitem;
+
+public class invoiceitemtest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
